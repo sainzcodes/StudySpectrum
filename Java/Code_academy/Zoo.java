21931531b3a898c567bc3209b1906297bb5191e8
Santiago <59790233+sainzcodes@users.noreply.github.com>
2023-08-18 10:06:06 -0600

    Another one

---
+++ Java/Code_academy/Zoo.java
@@ -0,0 +1,10 @@
+public class Zoo {
+    public static void main(String[] args){
+      int animals = 12;
+      String species = "zebra";
+
+      //Use + to make a new String variable called zooDescription.
+      String zooDescription = "Our zoo has " + animals + " " + species + "s!";
+      System.out.println(zooDescription);
+    }
+}
